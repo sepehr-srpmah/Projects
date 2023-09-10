0e55e14b8de31a301d087c0aa5caff0bcde2d02b
sepehr-srpmah <mahmoudisepehr61@gmail.com>
2023-09-10 21:49:33 +0430

    Commit Project

---
+++ EventExample/ProcessEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EventExample
+{
+    public class ProcessEventArgs
+    {
+        public bool IsSuccessful { get; set; }
+        public DateTime CompletionTime { get; set; }
+    }
+}
