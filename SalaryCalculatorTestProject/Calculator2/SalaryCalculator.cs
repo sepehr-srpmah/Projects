af49f6c4d92303bb9c997c544f67fabb9364c47c
srpmah <mahmoudisepehr61@gmail.com>
2023-09-19 19:09:40 +0430

    Commiting two projects ...

---
+++ SalaryCalculatorTestProject/Calculator2/SalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    public class SalaryCalculator
+    {
+        public decimal GetAnnualSalary(decimal hourlyWage)
+        {
+            const int HourInYear = 2080;
+            decimal annualSalary = hourlyWage * HourInYear;
+            return annualSalary;
+        }
+    }
+}
---
+++ SalaryCalculatorTestProject/SalaryCalculatorTestProject/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Calculator2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SalaryCalculatorTestProject
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void AnnualSalaryTest()
+        {
+            // Arrange
+            SalaryCalculator sc = new SalaryCalculator();
+
+            // Act
+            decimal annualSalary = sc.GetAnnualSalary(50);
+
+            // Assert
+            Assert.AreEqual(104000, annualSalary);
+        }
+    }
+}
